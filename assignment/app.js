03ed1d4f63c7281c58f19f49499e74e438c305b7
Danny <dgimeno777@gmail.com>
2017-08-10 00:23:09 -0400

    cleanup from testing

--- assignment/app.js
+++ assignment/app.js
@@ -2,24 +2,13 @@ module.exports = function (app) {
 
 
     var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
-    console.log("heyhey");
-    if (process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-        console.log("in1");
-        var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-        var password = process.env.MLAB_PASSWORD_WEBDEV;
-        console.log("in1.1");
-        connectionString = 'mongodb://' + username + ':' + password;
-        console.log("in1.2");
+    if (process.env.MONGODB_URI) { // check if running remotely
         connectionString = process.env.MONGODB_URI; // user yours
-        console.log("in1.3");
     }
-    console.log("in2");
     var mongoose = require("mongoose");
-    console.log("in3");
     mongoose.connect(connectionString);
-    console.log("in4");
+
     require("./model/model.server");
-    console.log("in5");
     require("./services/user.service.server.js")(app);
     require("./services/website.service.server.js")(app);
     require("./services/page.service.server.js")(app);
